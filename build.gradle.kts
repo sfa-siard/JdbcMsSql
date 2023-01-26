43e20aff9fae9e8166c17c3744d538178936290b
Max Burri <burri@puzzle.ch>
2023-01-26 11:31:31 +0100

    issue: test to reproduce the issue with multiple extended properties whit the same name on a table

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,19 +27,17 @@ sourceSets {
         java {
             setSrcDirs(listOf("test"))
         }
+
+        resources {
+            setSrcDirs(listOf("test/resources"))
+        }
     }
 }
 
 dependencies {
-    // Use JUnit test framework.
-    testImplementation("junit:junit:4.13.2")
-
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    //api("org.apache.commons:commons-math3:3.6.1")
-
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    // implementation("com.google.guava:guava:30.1.1-jre")
-
-    // legacy dependencies -
     implementation(fileTree("lib") { include("*.jar") })
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.testcontainers:testcontainers:1.19.0")
+    testImplementation("org.testcontainers:mssqlserver:1.19.0")
 }
