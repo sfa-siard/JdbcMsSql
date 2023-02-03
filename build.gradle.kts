42d5ed1e8de3d04d17c35518ba3d2a6a8e9448f7
Max Burri <burri@puzzle.ch>
2023-02-03 12:00:11 +0100

    moved generated files out of the lib folder

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,20 @@ repositories {
     mavenCentral()
 }
 
+sourceSets {
+    main {
+        java {
+            setSrcDirs(listOf("src"))
+        }
+    }
+
+    test {
+        java {
+            setSrcDirs(listOf("test"))
+        }
+    }
+}
+
 dependencies {
     // Use JUnit test framework.
     testImplementation("junit:junit:4.13.2")
@@ -24,8 +38,8 @@ dependencies {
     //api("org.apache.commons:commons-math3:3.6.1")
 
     // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-   // implementation("com.google.guava:guava:30.1.1-jre")
+    // implementation("com.google.guava:guava:30.1.1-jre")
 
     // legacy dependencies -
-    implementation fileTree(include: ['*.jar'], dir: 'lib')
+    implementation(fileTree("lib") { include("*.jar") })
 }
