586d248e0c5cecb42161010e19a0c0b74e816393
llorentelemmc <llorente@puzzle.ch>
2025-08-26 13:45:35 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ java {
 group = "ch.admin.bar"
 version = scmVersion.version
 val versions = mapOf(
-    "jdbc-base" to "v2.2.10",
+    "jdbc-base" to "v2.2.11",
 )
 
 repositories {
@@ -29,8 +29,8 @@ dependencies {
     implementation(fileTree("lib") { include("*.jar") })
 
     implementation("org.antlr:antlr4-runtime:4.5.2")
-    implementation("ch.admin.bar:enterutilities:v2.2.4")
-    implementation("ch.admin.bar:SqlParser:v2.2.3")
+    implementation("ch.admin.bar:enterutilities:v2.2.5")
+    implementation("ch.admin.bar:SqlParser:v2.2.4")
     implementation("ch.admin.bar:jdbc-base:${versions["jdbc-base"]}")
 
     //test dependencies
